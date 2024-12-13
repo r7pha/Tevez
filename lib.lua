c885e5d9fde584fcf95900e58589fde80f8df088
raphael <105987360+r7pha@users.noreply.github.com>
2024-12-13 17:24:41 -0300

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -4399,7 +4399,6 @@ function library:init()
 
     end
     
-    -- Watermark
     do
         self.watermark = {
             objects = {};
@@ -4407,36 +4406,36 @@ function library:init()
                 {"Zigga", true},
                 {"V"..getgenv().Config.Version, true},
                 {'0 fps', true},
-                {'0ms', true},
+                {'0ms', true}, 
             };
             lock = 'custom';
             position = newUDim2(0,0,0,0);
             refreshrate = 25;
         }
-
+    
         function self.watermark:Update()
             self.objects.background.Visible = library.flags.watermark_enabled
             if library.flags.watermark_enabled then
                 local date = {os.date('%b',os.time()), os.date('%d',os.time()), os.date('%Y',os.time())}
                 local daySuffix = math.floor(date[2]%10)
                 date[2] = date[2]..(daySuffix == 1 and 'st' or daySuffix == 2 and 'nd' or daySuffix == 3 and 'rd' or 'th')
-
-                self.text[4][1] = library.stats.fps..' fps'
-                self.text[5][1] = floor(library.stats.ping)..'ms'
-
-                local text = {};
+    
+                self.text[3][1] = library.stats.fps..' fps'
+                self.text[4][1] = floor(library.stats.ping)..'ms' 
+    
+                local text = {}
                 for _,v in next, self.text do
                     if v[2] then
-                        table.insert(text, v[1]);
+                        table.insert(text, v[1])
                     end
                 end
-
-                self.objects.text.Text = table.concat(text,' | ')
+    
+                self.objects.text.Text = table.concat(text, ' | ')
                 self.objects.background.Size = newUDim2(0, self.objects.text.TextBounds.X + 10, 0, 17)
-
-                local size = self.objects.background.Object.Size;
-                local screensize = workspace.CurrentCamera.ViewportSize;
-
+    
+                local size = self.objects.background.Object.Size
+                local screensize = workspace.CurrentCamera.ViewportSize
+    
                 self.position = (
                     self.lock == 'Top Right' and newUDim2(0, screensize.X - size.X - 15, 0, 15) or
                     self.lock == 'Top Left' and newUDim2(0, 15, 0, 15) or
@@ -4445,61 +4444,12 @@ function library:init()
                     self.lock == 'Top' and newUDim2(0, screensize.X / 2 - size.X / 2, 0, 15) or
                     newUDim2(library.flags.watermark_x / 100, 0, library.flags.watermark_y / 100, 0)
                 )
-
+    
                 self.objects.background.Position = self.position
             end
         end
-
-        do
-            local objs = self.watermark.objects;
-            local z = self.zindexOrder.watermark;
-            
-            objs.background = utility:Draw('Square', {
-                Visible = false;
-                Size = newUDim2(0, 200, 0, 17);
-                Position = newUDim2(0,800,0,100);
-                ThemeColor = 'Background';
-                ZIndex = z;
-            })
-
-            objs.border1 = utility:Draw('Square', {
-                Size = newUDim2(1,2,1,2);
-                Position = newUDim2(0,-1,0,-1);
-                ThemeColor = 'Border 2';
-                Parent = objs.background;
-                ZIndex = z-1;
-            })
-
-            objs.border2 = utility:Draw('Square', {
-                Size = newUDim2(1,2,1,2);
-                Position = newUDim2(0,-1,0,-1);
-                ThemeColor = 'Border 3';
-                Parent = objs.border1;
-                ZIndex = z-2;
-            })
-            
-            objs.topbar = utility:Draw('Square', {
-                Size = newUDim2(1,0,0,1);
-                ThemeColor = 'Accent';
-                ZIndex = z+1;
-                Parent = objs.background;
-            })
-
-            objs.text = utility:Draw('Text', {
-                Position = newUDim2(.5,0,0,2);
-                ThemeColor = 'Primary Text';
-                Text = 'Watermark Text';
-                Size = 13;
-                Font = 2;
-                ZIndex = z+1;
-                Outline = true;
-                Center = true;
-                Parent = objs.background;
-            })
-
-        end
     end
-
+    
     local lasttick = tick();
     utility:Connection(runservice.RenderStepped, function(step)
         library.stats.fps = floor(1/step)
