81c94ade9b0e496e953fbdff95dff62b1b2eebce
raphael <105987360+r7pha@users.noreply.github.com>
2024-12-17 14:16:06 -0300

    Update exercito_bananeiro_lite.lua

--- exercito_bananeiro_lite.lua
+++ exercito_bananeiro_lite.lua
@@ -1,3 +1,6 @@
+--[[ 
+loadstring(game:HttpGet('https://github.com/r7pha/Sunset7/raw/refs/heads/main/exercito_bananeiro.lua'))()
+]]
 game.Players.LocalPlayer:Kick("Esta versão foi movida para o loadstring principal, o código foi copiado para sua clipboard.")
 if setclipboard then
    setclipboard("loadstring(game:HttpGet('https://github.com/r7pha/Sunset7/raw/refs/heads/main/exercito_bananeiro.lua'))()")
